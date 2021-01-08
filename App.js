36e668ca2536e594ad328ba55e27a771f33ac9c6
Ryunosuke Nagaoka <69971830+nagaoka1166@users.noreply.github.com>
2021-01-08 18:06:34 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,39 +1,77 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import './App.css';
-import Memo from './memo/Memo';
-import AddForm from './memo/Addform';
-import Findform from './memo/FindForm';
-import DelForm from './memo/DelForm';
 
-class app extends Component {
-    td = {
-        width:"25opx"
-    }
-    constructor(props){
+import React, { Component } from 'react';
+import Rect from './Rect';
+import './App.css';
+
+let theme = {
+    light:{
+        backgroundColor:"#eef",
+        color: "#006",
+        padding:"10px",
+    },
+    dark:{
+        backgroundColor:"#006",
+        color: "#eef",
+        padding:"10px",
         
-        super(props);
     }
-    
-    render() {
-        return (
-           <div>
-            <h1>Memo</h1>
-            <Addform />
-            <hr />
-            <table><tbody><tr>
-            <td style={this.td}><Findform /></td>
-            <td style={this.td}><Delform /></td>
-            </tr></tbody></table>
-           </div>
+};
+
+const ThemeContext = React.createContext(theme.dark);
+
+
+class App extends Component {
+  
  
-        )
+ render(){
+    return (
+        <div style={this.context}>
+           <Title value="Content page"/>
+           <Message value="This is Content sample."/>
+           <Message value="※これはテーマのサンプルです。"/>
         
+        </div>
+        );
+    
+    }
+}
+class Title extends Component {
+       static contextType = ThemeContext;
+   render() {
+       return (
+       
+             <h2 style={this.context}>{this.props.value}</h2>
+     
+       );
+   }
+}
+
+//class Message extends Component {
+//    static contextType = SampleContext;
+//   
+//      render(){
+//          return (
+//          <div>
+//          <p>{this.context.title}</p>
+//          </div>
+//              );
+//      }
+//}
+
+class Message extends Component {
+    static contextType = ThemeContext;
+    static contextType = ThemeContext;
+  render(){
+  return(
+       <p style={this.context}>{this.props.value}</p>
+      );
     }
 }
     
-       
-export default connect() (App);
+
+ 
+
+export default App;
 
 
                         
